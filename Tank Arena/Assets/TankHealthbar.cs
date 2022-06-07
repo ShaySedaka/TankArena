4d9794223f3eaf5fb437bc4ecf0efbed3556bdb6
Shay Sedaka <shays@justplay.lol>
2022-06-07 21:16:00 +0300

    Health Bar

---
+++ Tank Arena/Assets/TankHealthbar.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TankHealthbar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+
+    [SerializeField] private Tank _tank;
+
+    [SerializeField] private Camera _mainCamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _slider.maxValue = _tank.MaxHelath;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _slider.value = _tank.CurrentHelath;
+
+        this.transform.LookAt(_mainCamera.transform);
+    }
+
+
+}
