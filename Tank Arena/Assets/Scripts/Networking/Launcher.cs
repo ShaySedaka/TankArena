9245543bc535ee185541612bd0d572deaa079f88
Shay Sedaka <shays@justplay.lol>
2022-06-14 21:00:18 +0300

    Some Photon Progress

---
+++ Tank Arena/Assets/Scripts/Networking/Launcher.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Private Serializable Fields
+
+    [SerializeField] private byte maxPlayersPerRoom = 4;
+
+    [SerializeField] private UIManager _uiManager;
+
+    #endregion
+
+
+    #region Private Fields
+
+
+    /// <summary>
+    /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
+    /// </summary>
+    string gameVersion = "1";
+
+
+    #endregion
+
+
+    #region MonoBehaviour CallBacks
+
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
+    /// </summary>
+    void Awake()
+    {
+        // #Critical
+        // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+
+    /// <summary>
+    /// MonoBehaviour method called on GameObject by Unity during initialization phase.
+    /// </summary>
+    void Start()
+    {
+        Connect();
+
+    }
+
+
+    #endregion
+
+
+    #region Public Methods
+
+
+    /// <summary>
+    /// Start the connection process.
+    /// - If already connected, we attempt joining a random room
+    /// - if not yet connected, Connect this application instance to Photon Cloud Network
+    /// </summary>
+    public void Connect()
+    {
+        // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
+        if (PhotonNetwork.IsConnected)
+        {
+            
+        }
+        else
+        {
+            // #Critical, we must first and foremost connect to Photon Online Server.
+            PhotonNetwork.ConnectUsingSettings();
+            PhotonNetwork.GameVersion = gameVersion;
+
+            _uiManager.OnConnecting();
+        }
+    }
+
+    public void StartMatchmaking()
+    {
+        // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    #endregion
+
+    #region MonoBehaviourPunCallbacks Callbacks
+
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to master.");
+        _uiManager.OnConnected();
+    }
+
+
+    public override void OnDisconnected(DisconnectCause cause)
+    {
+        Debug.LogWarningFormat("OnDisconnected() was called by PUN with reason {0}", cause);
+        _uiManager.OnConnectionError();
+    }
+
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log("OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
+
+        // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
+        PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+    }
+
+    public override void OnJoinedRoom()
+    {
+        Debug.Log("OnJoinedRoom() called by PUN. Now this client is in a room.");
+
+        // #Critical: We only load if we are the first player, else we rely on `PhotonNetwork.AutomaticallySyncScene` to sync our instance scene.
+        if (PhotonNetwork.CurrentRoom.PlayerCount == 1)
+        {
+            Debug.Log("First one in the room, loading Arena...");
+
+
+            // #Critical
+            // Load the Room Level.
+            PhotonNetwork.LoadLevel(1);
+        }
+    }
+
+    #endregion
+
+}
