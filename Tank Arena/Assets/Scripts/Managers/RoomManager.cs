71fdc027c833bbc0f8aa00899ce690af9600991e
Shay Sedaka <shays@justplay.lol>
2022-07-13 20:37:19 +0300

    Deleted Photon Demos

--- Tank Arena/Assets/Scripts/Managers/RoomManager.cs
+++ Tank Arena/Assets/Scripts/Managers/RoomManager.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Photon.Pun;
 using Photon.Realtime;
-
+using System.IO;
 
 public class RoomManager : MonoBehaviourPunCallbacks
 {
 
+    public static RoomManager Instance;
+
+    private void Awake()
+    {
+        if(Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        DontDestroyOnLoad(gameObject);
+            Instance = this;
+    }
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+
+    }
+
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        if(scene.buildIndex == 1)
+        {
+            PhotonNetwork.Instantiate(Path.Combine("PhotonPrefabs", "PlayerManager"), Vector3.zero, Quaternion.identity);
+        }
+    }
 
     #region Photon Callbacks
-
-
     /// <summary>
     /// Called when the local player left the room. We need to load the launcher scene.
     /// </summary>
